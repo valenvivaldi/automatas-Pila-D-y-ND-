a93ece788737685130787055aa315aba954d5a1b
Valentin Vivaldi <valenvivaldi@gmail.com>
2017-05-30 17:01:22 -0300

    First commit

---
+++ src/automata/AP.java
@@ -0,0 +1,90 @@
+package automata;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Stack;
+import utils.Quintuple;
+
+
+
+public abstract class AP {
+
+	public static final Character Lambda = '_';
+	public static final Character Joker = '@';
+	public static final Character Initial = 'Z';
+
+	protected State initial;
+    protected Character stackInitial;
+    protected Stack<Character> stack; //stack of the automaton
+    protected Set<State> states;
+    protected Set<Character> alphabet;
+    protected Set<Character> stackAlphabet; //Alphabet of the stack
+    protected Set<Quintuple<State,Character,Character,String,State>> transitions; //delta function
+    protected Set<State> finalStates;
+
+
+    /*
+     * A static constructor should be implemented depending on the final design of the automaton
+     * */
+
+
+    public static State getElemFromSet(Set<State> q,State o){
+		  Iterator<State> iterator=  q.iterator();
+      while (iterator.hasNext()){
+        State elementOfSet = iterator.next();
+        if (elementOfSet.equals(o)){
+          return elementOfSet;
+        }
+
+      }
+      return null;
+
+     }
+
+    public Set<State> final_states(){
+        return finalStates;
+    }
+
+    public State initial_state(){
+        return initial;
+    }
+
+    public Set<Character> alphabet(){
+        return alphabet;
+    }
+
+    public Set<State> states(){
+        return states;
+    }
+
+    public final String to_dot(){
+        //assert rep_ok();
+        char quotes= '"';
+        Iterator i;
+        String aux;
+        aux = "digraph{\n";
+        aux = aux + "inic[shape=point];\n" + "inic->" + this.initial.name() + ";\n";
+        i = this.transitions.iterator();
+        while (i.hasNext()) {
+           Quintuple quintuple =(Quintuple) i.next();
+           aux = aux + quintuple.first().toString() + "->" + quintuple.fifth().toString() + " [label=" +quotes+ quintuple.second().toString() +"/"+ quintuple.third()+"/"+quintuple.fourth()+ quotes+ "];\n";
+        }
+        aux = aux+ "\n";
+        i = this.finalStates.iterator();
+        while (i.hasNext()){
+            State estado = (State) i.next();
+            aux = aux + estado.name() + "[shape=doublecircle];\n";
+        }
+        aux = aux + "}";
+        return aux;
+    }
+
+    /**
+    * this methods should be implemented in DFAPila
+    */
+    public abstract boolean accepts(String string);
+
+    public abstract Object delta(State from, Character c);
+
+
+}
---
+++ src/automata/DFAPila.java
@@ -0,0 +1,138 @@
+package automata;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Stack;
+
+import utils.Quintuple;
+
+public final class DFAPila extends AP {
+
+private Object nroStates[];
+private Stack<Character> stack;     //the stack of the automaton
+
+
+/**
+ * Constructor of the class - returns a DFAPila object
+ * @param states - states of the DFAPila
+ * @param alphabet - the alphabet of the automaton
+ * @param stackAlphabet - the alphabet of the stack
+ * @param transitions - transitions of the automaton
+ * @param stackInitial - a Character which represents the initial element of the stack
+ * @param initial - initial State of the automaton
+ * @param final_states - acceptance states of the automaton
+ * @throws IllegalArgumentException
+ */
+public DFAPila(
+        Set<State> states,
+        Set<Character> alphabet,
+        Set<Character> stackAlphabet,
+        Set<Quintuple<State, Character,Character,String, State> > transitions,
+        Character stackInitial,
+        State initial,
+        Set<State> final_states)
+throws IllegalArgumentException
+{
+        this.states = states;
+        this.alphabet = alphabet;
+        this.stackAlphabet = stackAlphabet;
+        
+       
+    	//public static final Character Initial = 'Z';
+        
+        stackAlphabet.add(Lambda); // _  the character lambda is used in the stack to know when do a pop
+        
+        stackAlphabet.add(Joker); //  @   the mark of the stack
+        this.transitions = transitions;
+        this.stackInitial = stackInitial;
+        this.initial = initial;
+        this.finalStates = final_states;
+        nroStates =  states.toArray();
+        stack = new Stack<Character>();
+        stack.add(stackInitial); //insert the mark in the stack ANTES IBA JOKER
+        if (!rep_ok()) {
+                throw new  IllegalArgumentException();
+        }
+        System.out.println("Is a DFA Pila");
+}
+
+
+@Override
+public State delta(State from, Character c){
+		System.out.println("Se buscara un quintuple compatible para el caracter: "+c+" y el estado actual: "+from.name()+" PILA: "+stack.toString());
+        Iterator<Quintuple<State, Character,Character,String, State> > iterator =  this.transitions.iterator();
+        Quintuple<State, Character,Character,String, State> quintupleCompatible = null;
+        while ((quintupleCompatible ==null )&&iterator.hasNext()) {
+        	
+        		Quintuple<State, Character,Character,String, State> quintupleCandidato = iterator.next();
+        		//System.out.println("quintuple candidato: "+quintupleCandidato.toString());
+        		if (quintupleCandidato.first().equals(from)&&quintupleCandidato.second().equals(c)&&(quintupleCandidato.third().equals(this.stack.peek())||quintupleCandidato.third().equals(Joker))  ) {
+                        quintupleCompatible = quintupleCandidato;
+                }
+        }
+        if(quintupleCompatible != null) {
+        	//System.out.println("Se encontro un quintuple compatible para el caracter: "+c+" y el estado actual: "+from.name());
+        	System.out.println("El quintuple es: "+quintupleCompatible.toString());
+        	stack.pop();
+        	
+                String stringParaApilar =quintupleCompatible.fourth();
+                int i =stringParaApilar.length()-1;
+                
+                
+                while(i>=0&&stringParaApilar.charAt(i)!=Lambda) {
+                    System.out.println(stack.toString());    
+                	stack.push(stringParaApilar.charAt(i));
+                        i--;
+                }	
+                
+                System.out.println("Stack: "+stack.toString());
+                return quintupleCompatible.fifth();
+        }
+
+        return null;
+}
+
+@Override
+public boolean accepts(String cadena) {
+        
+        State estadoActual = this.initial;
+        int indiceString = 0;
+        while((indiceString<cadena.length()) && estadoActual!=null) {
+                estadoActual=delta(estadoActual, cadena.charAt(indiceString));
+                indiceString++;
+        }
+        
+        //System.out.println("estados finales: "+this.finalStates().toString());
+        if(indiceString==cadena.length()){
+        	if (stack.empty()) {
+            	System.out.println("Por pila vacia fue aceptado");
+                    return true;
+            }
+            
+            System.out.println("verif estado fial");
+            Iterator<State> iteradorestadosfinales =finalStates.iterator();
+           	while (iteradorestadosfinales.hasNext()){
+            	if (iteradorestadosfinales.next().equals(estadoActual)) {
+            		System.out.println("Por estado final fue aceptado");
+            		return true;
+            	}
+            }
+        }
+        
+        	
+        	
+        System.out.println("no acepto por ninguno");
+        
+        return false;
+}
+
+public boolean rep_ok() {
+		
+	
+	
+	
+        return true;
+}
+
+
+}
